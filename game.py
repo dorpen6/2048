c06938cd1b7ba0cbfacd9e11697d370f9e53b192
dorpen6 <alivalishov20@gmail.com>
2025-05-24 03:43:50 +0200

    DRAWING GRIDS

--- game.py
+++ game.py
@@ -6,12 +6,12 @@ pygame.init()
 
 FPS = 60
 
-WIDTH, HEIGHT = 800, 800
-ROWS = 4
-COLUMNS = 4
+WINDOW_WIDTH, WINDOW_HEIGHT = 800, 800
+NUMBER_OF_ROWS = 4
+NUMBER_OF_COLUMNS = 4
 
-RECT_HEIGHT = HEIGHT // ROWS
-RECT_WIDTH = WIDTH // COLUMNS
+RECT_HEIGHT = WINDOW_HEIGHT // NUMBER_OF_ROWS
+RECT_WIDTH = WINDOW_WIDTH // NUMBER_OF_COLUMNS
 
 OUTLINE_COLOR = (187, 173, 160)
 OUTLINE_THICKNESS = 10
@@ -21,19 +21,22 @@ FONT_COLOR = (119, 110, 101)
 FONT = pygame.font.SysFont("comicsans", 60, bold=True)
 MOVE_VELOCITY = 20
 
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("2048")
+WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("2048")        
+
+class Tile:
+    pass
 
 def draw_grid(window):
-    for row in range(1, ROWS):
+    for row in range(1, NUMBER_OF_ROWS):
         y = row * RECT_HEIGHT
-        pygame.draw.line(window, OUTLINE_COLOR, (0, y), (WIDTH, y), OUTLINE_THICKNESS)
+        pygame.draw.line(window, OUTLINE_COLOR, (0, y), (WINDOW_WIDTH, y), OUTLINE_THICKNESS)
     
-    for column in range(1, COLUMNS):
+    for column in range(1, NUMBER_OF_COLUMNS):
         x = column * RECT_WIDTH
-        pygame.draw.line(window, OUTLINE_COLOR, (x, 0), (x, HEIGHT), OUTLINE_THICKNESS)
+        pygame.draw.line(window, OUTLINE_COLOR, (x, 0), (x, WINDOW_HEIGHT), OUTLINE_THICKNESS)
 
-    pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
+    pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WINDOW_WIDTH, WINDOW_HEIGHT), OUTLINE_THICKNESS)
 
 def draw(window):
      window.fill(BACKGROUND_COLIR)
